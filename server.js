9a36729b78a48f0b623c2f07d6bf0eee80aa4460
gabrielbaron16 <gabrielbu161@gmail.com>
2020-06-11 10:24:00 -0400

    Configuración para Despliegue Automático en Heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/merak-medical-inventory'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/merak-medical-inventory/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
