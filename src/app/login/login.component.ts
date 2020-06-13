9b343d543a99f6c1ebe06ce035282ec60b640fc6
gabrielbaron16 <gabrielbu161@gmail.com>
2020-06-13 10:08:27 -0400

    Componente de Login Junto con su respectivo module y routing-module

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  submitted = false;
+  isLoading = false;
+  LoginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get f() {
+    return this.LoginForm.controls;
+  }
+
+}
