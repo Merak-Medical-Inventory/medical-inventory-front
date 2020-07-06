3d95731194488c06a29c749dd8dc851e2e3f4f1a
gabrielbaron16 <gabrielbu161@gmail.com>
2020-07-05 20:44:45 -0400

    Se agregó Landing Page y Modulo de Layout

---
+++ src/app/components/layout/layout.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent} from './sidebar/sidebar.component';
+import { LandingpageComponent} from './landingpage/landingpage.component';
+import {LayoutRoutingModule} from './layout-routing.module';
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    LandingpageComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule { }
