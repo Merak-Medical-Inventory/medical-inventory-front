e8bf761f30a05ef2a50abb49f78d1b8bf1724694
gabrielbaron16 <gabrielbu161@gmail.com>
2020-07-08 14:46:33 -0400

    Entidad Usuario

---
+++ src/app/entities/user.ts
@@ -0,0 +1,12 @@
+export interface UserLogin {
+  username: string;
+  password: string;
+}
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  name: string;
+  lastname: string;
+}
