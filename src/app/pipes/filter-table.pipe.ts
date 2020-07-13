d091fce3427de33af16a57de0cf5db808296318c
gabrielbaron16 <gabrielbu161@gmail.com>
2020-07-13 19:09:13 -0400

    Pipe para Filtrar Tablas

---
+++ src/app/pipes/filter-table.pipe.ts
@@ -0,0 +1,27 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'filterTable'
+})
+export class FilterTablePipe implements PipeTransform {
+
+  transform<T>(object: T[], searchValue: string): T[] {
+    if (!searchValue) {
+      return object;
+    }
+
+    return  object.filter(value => {
+      for (const key in value) {
+        if (value.hasOwnProperty(key)) {
+          if (value[key] != null) {
+            if (value[key].toString().toLowerCase().indexOf(searchValue.toLowerCase()) > -1) {
+              return true;
+            }
+          }
+        }
+      }
+    });
+
+  }
+
+}
