0c959ab79016952077cef928c8b6275629d2366f
gabrielbaron16 <gabrielbu161@gmail.com>
2020-07-15 21:47:25 -0400

    Se Agregaron los Servicios para Consultar y Agregar Categorías

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {PostCategory} from '../../entities/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  getCategories() {
+    return this.http.get(`${environment.baseUrl}category`, {observe: 'response'});
+  }
+
+  getCategoryById(id: number) {
+    return this.http.get(`${environment.baseUrl}category/${id}`, {observe: 'response'});
+  }
+
+  postCategory(body: PostCategory) {
+    return this.http.post(`${environment.baseUrl}category`, body, {observe: 'response'});
+  }
+}
