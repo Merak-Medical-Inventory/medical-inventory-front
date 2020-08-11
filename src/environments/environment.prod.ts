d6be53a977882a9d5851d9da0defc55fa05976fb
gabrielbaron16 <gabrielbu161@gmail.com>
2020-08-10 23:51:45 -0400

    Valores de Entorno de Producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  authServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8080/',
+  inventoryServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8081/'
 };
