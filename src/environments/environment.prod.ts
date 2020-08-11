5500f2d06cd62960c3a47085b1e0c2e184daaa2b
gabrielbaron16 <gabrielbu161@gmail.com>
2020-08-11 12:04:10 -0400

    Implementación en Producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8080/',
-  inventoryServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8081/'
+  authServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8080/api/',
+  inventoryServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8081/api/'
 };
