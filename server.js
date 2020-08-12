58d7c1a36fb0a9c6a63f6099ad2717126a49a6b6
gabrielbaron16 <gabrielbu161@gmail.com>
2020-08-11 22:32:42 -0400

    Implementación en Producción

--- server.js
+++ server.js
@@ -7,12 +7,8 @@ const path = require('path');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/merak-medical-inventory'));
 
-app.get('', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/merak-medical-inventory/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
