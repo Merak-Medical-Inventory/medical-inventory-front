ec1d685998f3fe8ecb1012bdbfdb5c95edefd029
gabrielbaron16 <gabrielbu161@gmail.com>
2020-08-12 23:40:05 -0400

    Modificando el Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8080/api/',
-  inventoryServiceUrl: 'http://ec2-18-233-65-25.compute-1.amazonaws.com:8081/api/'
+  authServiceUrl: 'http://ec2-34-227-93-6.compute-1.amazonaws.com:8080/api/',
+  inventoryServiceUrl: 'http://ec2-34-227-93-6.compute-1.amazonaws.com:8081/api/'
 };
