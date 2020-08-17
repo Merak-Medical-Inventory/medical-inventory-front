f4d2a3789bd5a53c686d566e2d2e1374bb65d8cf
gabrielbaron16 <gabrielbu161@gmail.com>
2020-08-16 21:38:44 -0400

    Se añadió las rutas de los servicios en heroku en el environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authServiceUrl: 'https://merak-auth-dev.herokuapp.com/',
-  inventoryServiceUrl: 'https://merak-inventory-dev.herokuapp.com/'
+  authServiceUrl: 'https://merak-auth-dev.herokuapp.com/api/',
+  inventoryServiceUrl: 'https://merak-inventory-dev.herokuapp.com/api/'
 };
